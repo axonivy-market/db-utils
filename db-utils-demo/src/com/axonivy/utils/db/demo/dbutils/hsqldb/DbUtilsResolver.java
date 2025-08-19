7329b3dfe7bd159475a372bc581c17684c8942fa
quanpham-axonivy <quan.phamvan@axonactive.com>
2025-08-19 10:41:28 +0700

    test

--- db-utils-demo/src/com/axonivy/utils/db/demo/dbutils/hsqldb/DbUtilsResolver.java
+++ db-utils-demo/src/com/axonivy/utils/db/demo/dbutils/hsqldb/DbUtilsResolver.java
@@ -1,6 +1,9 @@
 package com.axonivy.utils.db.demo.dbutils.hsqldb;
 
 import com.axonivy.utils.db.resolver.HSQLDbUtilsResolver;
+
+import ch.ivyteam.ivy.environment.Ivy;
+
 import org.dbunit.dataset.Column;
 
 
@@ -9,8 +12,10 @@ import org.dbunit.dataset.Column;
  */
 public class DbUtilsResolver extends HSQLDbUtilsResolver {
 	private static final DbUtilsResolver INSTANCE = new DbUtilsResolver();
+	private static final Column COLUMN = new Column(null, null);
 
 	public static DbUtilsResolver getInstance() {
+		Ivy.log().info(COLUMN);
 		return INSTANCE;
 	}
 }
